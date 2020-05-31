6f6ad5f3ed150e9485f2fbb45e4c18ccf94d6c16
Елена <veintitress@yandex.ru>
2020-05-31 15:30:24 +0300

    Исправляет замечания наставника

--- js/stat.js
+++ js/stat.js
@@ -5,35 +5,40 @@ var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var GAP = 10;
+var IDENTATION_INDEX = 2;
 var CLOUD_COLOR = '#fff';
 var SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
+var TEXT_FONT = 'PT Mono';
 var CLOUD_FONT_SIZE = 16;
+var TEXT_COLOR = '#000';
+var TEXT_BASELINE = 'hanging';
+var YOUR_BAR_COLOR = 'rgba(255, 0, 0, 1)';
 var BAR_MAX_HEIGHT = 150;
 var BAR_WIDTH = 40;
 var BAR_GAP = 50;
-var CLOUD_GREETING = ['Ура вы победили!', 'Список результатов:'];
+var CLOUD_GREETINGS = ['Ура вы победили!', 'Список результатов:'];
+var USER_NAME = 'Вы';
 
 
 var renderText = function (ctx, text, positionX, positionY) {
-  ctx.fillStyle = '#000';
-  ctx.font = CLOUD_FONT_SIZE + 'px PT Mono';
-  ctx.textBaseline = 'hanging';
+  ctx.fillStyle = TEXT_COLOR;
+  ctx.font = CLOUD_FONT_SIZE + 'px ' + TEXT_FONT;
+  ctx.textBaseline = TEXT_BASELINE;
   ctx.fillText(text, positionX, positionY);
 };
 
 var renderCloud = function (ctx, positionX, positionY) {
   ctx.fillStyle = SHADOW_COLOR;
-  ctx.textBaseline = 'hanging';
   ctx.fillRect(positionX + GAP, positionY + GAP, CLOUD_WIDTH, CLOUD_HEIGHT);
   ctx.fillStyle = CLOUD_COLOR;
   ctx.fillRect(positionX, positionY, CLOUD_WIDTH, CLOUD_HEIGHT);
-  for (var i = 0; i < CLOUD_GREETING.length; i++) {
-    renderText(ctx, CLOUD_GREETING[i], CLOUD_X + GAP * 2, CLOUD_Y + GAP * 2 + CLOUD_FONT_SIZE * i);
+  for (var i = 0; i < CLOUD_GREETINGS.length; i++) {
+    renderText(ctx, CLOUD_GREETINGS[i], CLOUD_X + GAP * IDENTATION_INDEX, CLOUD_Y + GAP * IDENTATION_INDEX + CLOUD_FONT_SIZE * i);
   }
 };
 
 var getRandom = function (number) {
-  return Math.round(Math.random() * number);
+  return Math.random() * number;
 };
 
 var getMaxElement = function (arr) {
@@ -52,8 +57,9 @@ var renderHistogram = function (ctx, playersNames, playersTimes) {
     var barX = CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i;
     var barY = CLOUD_HEIGHT - BAR_MAX_HEIGHT - GAP - CLOUD_FONT_SIZE;
     var barHeight = (BAR_MAX_HEIGHT * playersTimes[i]) / maxTime;
-    var colorBar = playersNames[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'hsl(240, ' + getRandom(100) + '%, 50%)';
-    renderText(ctx, getRandom(playersTimes[i]), barX, barY + BAR_MAX_HEIGHT - barHeight - GAP * 2);
+    var otherBarsColor = 'hsl(240, ' + getRandom(100) + '%, 50%)';
+    var colorBar = playersNames[i] === USER_NAME ? YOUR_BAR_COLOR : otherBarsColor;
+    renderText(ctx, Math.round(getRandom(playersTimes[i])), barX, barY + BAR_MAX_HEIGHT - barHeight - GAP * IDENTATION_INDEX);
     ctx.fillStyle = colorBar;
     ctx.fillRect(barX, barY + BAR_MAX_HEIGHT - barHeight, BAR_WIDTH, barHeight);
     renderText(ctx, playersNames[i], barX, barY + BAR_MAX_HEIGHT + GAP);
